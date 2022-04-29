fbc826db928cdf14491926424d31ffec76431126
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-04-29 23:33:19 +0000

    Sync LeetCode submission - Group Anagrams (javascript)

--- problems/group_anagrams/solution.js
+++ problems/group_anagrams/solution.js
@@ -4,13 +4,16 @@
  */
 var groupAnagrams = function(strs) {
     let map = {};
-    for (let str of strs) {
+    
+    for (let i = 0; i < strs.length; i++) {
+        let str = strs[i];
         let count = new Array(26).fill(0);
         for (let c of str) {
             count[c.charCodeAt()-97]++;
         }
-        let key = count.join('#');
+        let key = count.join('-');
         map[key] ? map[key].push(str) : map[key] = [str];
     }
+    
     return Object.values(map);
 };
