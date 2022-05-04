1c126b7516bfc99b587aa42393b3b0da155fcbde
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-05-04 03:52:12 +0000

    Sync LeetCode submission - Linked List Cycle (javascript)

--- problems/linked_list_cycle/solution.js
+++ problems/linked_list_cycle/solution.js
@@ -10,17 +10,14 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {
-    if (head) {
-        let visited = new Set();
-        while (head.next) {
-            if (visited.has(head)) {
-                return true;
-            }
-            visited.add(head);
-            head = head.next;
-    }
-        return false;
+var hasCycle = function(head) {    
+    let slow = head;
+    let fast = head;
+    
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) return true;
     }
     return false;
 };
