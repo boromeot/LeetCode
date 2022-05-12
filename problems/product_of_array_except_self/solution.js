64f2bde6ceb901ab944a1f4266a9dba6a197b49d
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-05-12 21:51:31 +0000

    Sync LeetCode submission - Product of Array Except Self (javascript)

--- problems/product_of_array_except_self/solution.js
+++ problems/product_of_array_except_self/solution.js
@@ -3,25 +3,25 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let res = [];
+    const left = [];
     let product = 1;
-    let j = nums.length - 1;
     for (let i = 0; i < nums.length; i++) {
-        res.push(product);
+        left.push(product);
         product *= nums[i];
     }
+    
+    const res = [];
     product = 1;
     for (let j = nums.length - 1; j >= 0; j--) {
-        res[j] *= product;
+        left[j] *= product;
         product *= nums[j];
     }
-    return res;
+    return left;
 };
 //[ 1,  2, 3, 4]
 
 //[ 1,  1, 2, 6]
 //  0   1  2  3
-
 
 //[1, 4, 12, 24]
 //-4 -3  -2  -1
