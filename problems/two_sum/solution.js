b653aef142a600cc043bb93a2783ef9b7d218b21
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-05-12 22:01:40 +0000

    Sync LeetCode submission - Two Sum (javascript)

--- problems/two_sum/solution.js
+++ problems/two_sum/solution.js
@@ -4,15 +4,12 @@
  * @return {number[]}
  */
 function twoSum(nums, target) {
-   let map = new Map();
-    
+    const map = new Map();
     for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i];
+        const diff = target - nums[i];
         if (map.has(diff)) {
             return [map.get(diff), i];
         }
         map.set(nums[i], i);
     }
 }
-// 1 3 4 2
-// 1 3 4 2, target = 6
