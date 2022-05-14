1219f0520c15a319336072de591b3d8a800afda4
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-05-14 22:21:42 +0000

    Sync LeetCode submission - Binary Search (javascript)

--- problems/binary_search/solution.js
+++ problems/binary_search/solution.js
@@ -4,17 +4,18 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let l = 0;
-    let r = nums.length - 1;
+    let l = 0,
+        r = nums.length - 1;
+    
     while (l <= r) {
-        let m = Math.floor((l + r) / 2);
+        let m = Math.trunc((r + l) / 2);
         if (nums[m] === target) {
             return m;
         } else if (nums[m] < target) {
             l = m + 1;
-        } else if (nums[m] > target) {
+        } else {
             r = m - 1;
         }
-    } 
+    }
     return -1;
 };
