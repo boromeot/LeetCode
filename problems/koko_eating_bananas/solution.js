0f0325c2fe0d5af78b20de800b256d769e1d59da
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-05-14 22:34:33 +0000

    Sync LeetCode submission - Koko Eating Bananas (javascript)

--- problems/koko_eating_bananas/solution.js
+++ problems/koko_eating_bananas/solution.js
@@ -4,25 +4,19 @@
  * @return {number}
  */
 var minEatingSpeed = function(piles, h) {
-    if (h === piles.length) {
-        return Math.max(...piles);
-    }
-    
-    let l = 1;    
-    let r = Math.max(...piles); // O(n) # n being length of piles
-    
+    let l = 1,
+        r = Math.max(...piles);
     let res = r;
-    
     while (l <= r) {
-        let m = Math.floor((l + r) / 2);
+        let m = Math.trunc((l + r) / 2);
         let t = 0;
         for (let i = 0; i < piles.length; i++) {
             t += Math.ceil(piles[i] / m);
         }
         if (t <= h) {
-            r = m - 1;
             res = Math.min(res, m);
-        } else if (t > h) {
+            r = m - 1;
+        } else {
             l = m + 1;
         }
     }
