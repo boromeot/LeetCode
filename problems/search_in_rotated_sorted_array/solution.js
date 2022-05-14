c876ab2118c0f7774afef3dc1384bf4f432cfb32
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-05-14 23:05:47 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (javascript)

--- problems/search_in_rotated_sorted_array/solution.js
+++ problems/search_in_rotated_sorted_array/solution.js
@@ -4,27 +4,28 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let l = 0;
-    let r = nums.length - 1;
+    let l = 0,
+        r = nums.length - 1;
+    
     while (l <= r) {
         let m = Math.trunc((l + r) / 2);
-        if (nums[m] === target) {
-            return m;
-        } 
+        if (nums[m] === target) return m;
         
         if (nums[l] <= nums[m]) {
-            if (target > nums[m] || target < nums[l]) {
+            if (nums[l] <= target && target <= nums[m]) {
+                r = m - 1;
+            } else {
+                l = m + 1;             
+            }
+        } else {
+            if (nums[m] <= target && target <= nums[r]) {
                 l = m + 1;
             } else {
                 r = m - 1;
             }
-        } else {
-            if (target < nums[m] || target > nums[r]) {
-                r = m - 1;                
-            } else {
-                l = m + 1;
-            }
         }
     }
     return -1;
-}; // [5,6,7,0,1,2,4] 1
+}; // [4,5,6,7,0,1,2] 1
+//     l     m t   r
+//           l m
