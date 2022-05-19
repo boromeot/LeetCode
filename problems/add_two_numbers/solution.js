e09348402c5170580f1de15b216a30f024304220
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-05-19 00:37:31 +0000

    Sync LeetCode submission - Add Two Numbers (javascript)

--- problems/add_two_numbers/solution.js
+++ problems/add_two_numbers/solution.js
@@ -13,25 +13,21 @@
 var addTwoNumbers = function(l1, l2) {
     let dummy = new ListNode();
     let curr = dummy;
-    
     let carry = 0;
-    let v1;
-    let v2;
+    
     while (l1 || l2 || carry) {
+        let v1, v2;
         if (l1) {v1 = l1.val} else {v1 = 0};
         if (l2) {v2 = l2.val} else {v2 = 0};
         
-        
         let sum = v1 + v2 + carry;
         carry = Math.floor(sum / 10);
-        sum = sum % 10;
-        curr.next = new ListNode(sum);
+        curr.next = new ListNode(sum % 10);
+        curr = curr.next
         
-        curr = curr.next;
         if (l1) l1 = l1.next;
         if (l2) l2 = l2.next;
     }
-    
     return dummy.next;
 };
 
