cbcf52301782b3cc588f2ef75842f8d79acbbdd8
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-03 02:37:26 +0000

    Sync LeetCode submission - Contains Duplicate (javascript)

--- problems/contains_duplicate/solution.js
+++ problems/contains_duplicate/solution.js
@@ -3,14 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let set = new Set();
+    const set = new Set();
     
     for (let i = 0; i < nums.length; i++) {
-        let n = nums[i];
-        if (set.has(n)) {
-            return true;
-        }
-        set.add(n)
+        if (set.has(nums[i])) return true;
+        set.add(nums[i]);
     }
     return false;
-}
+    
+};
