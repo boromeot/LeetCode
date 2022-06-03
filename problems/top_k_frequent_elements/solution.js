8aa563ee4e52431f864ec75b3a58a10397fb5447
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-03 03:39:25 +0000

    Sync LeetCode submission - Top K Frequent Elements (javascript)

--- problems/top_k_frequent_elements/solution.js
+++ problems/top_k_frequent_elements/solution.js
@@ -4,25 +4,34 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    let map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        map.get(num) ? map.set(num, map.get(num) + 1) : map.set(num, 1);
-    }
-    const arr = new Array(nums.length);
-    for (let [k, v] of map) {
-        arr[v] ? arr[v].push(k) : arr[v] = [k];
-    }
-
     const res = [];
-    for (let j = arr.length - 1; j >= 0; j--) {
-        if (arr[j]) {
-            for (let o = 0; o < arr[j].length; o++) {
-            res.push(arr[j][o]);
-                if (res.length >= k) {
-                    return res;
-                }
-            }
-        }     
+    const bucket = new Array(nums.length);
+    
+    const map = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        const n = nums[i];
+        if (map.has(n)) {
+            map.set(n, map.get(n) + 1);
+        } else {
+            map.set(n, 1);
+        }
     }
-};  
+    
+    for (let [k, v] of map) {
+        if (bucket[v]) {
+            bucket[v].push(k);
+        } else {
+            bucket[v] = [k];
+        }
+    }
+    
+    for (let i = bucket.length - 1; i >= 0; i--) {
+        if (bucket[i]) {
+            for (let j = 0; j < bucket[i].length; j++) {
+                res.push(bucket[i][j]);
+                if (res.length === k) return res;
+            }
+        }
+    }    
+};
