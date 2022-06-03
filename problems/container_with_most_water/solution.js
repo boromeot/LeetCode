59b60dae8e94386e8d3ed5c487d3ac540f872de9
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-03 07:13:42 +0000

    Sync LeetCode submission - Container With Most Water (javascript)

--- problems/container_with_most_water/solution.js
+++ problems/container_with_most_water/solution.js
@@ -4,19 +4,22 @@
  */
 var maxArea = function(height) {
     let max = 0;
+    
     let l = 0,
         r = height.length - 1;
+    // height = Math.min(height[l], height[r]) -> Math.min(1, 7) -> 1
+    // width = r - l -> 8 - 0 -> 8
     
     while (l < r) {
-        let minHeight = Math.min(height[l], height[r]);
-        max = Math.max(max, minHeight * (r - l));
+        let h = Math.min(height[l], height[r]);
+        let w = r - l;
+        let area = h * w;
+        max = Math.max(max, area);
         if (height[l] < height[r]) {
             l++;
         } else {
-            r--;
+            r--;        
         }
     }
     return max;
 };
-    // [1,8,6,2,5,4,8,3,7]
-    //  0 1 2 3 4 5 6 7 8
