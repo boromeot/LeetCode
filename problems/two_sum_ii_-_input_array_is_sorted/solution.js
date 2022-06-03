ad6395e5f89ee80d5f5d683c542e37a4ac8bdea5
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-03 06:26:42 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted (javascript)

--- problems/two_sum_ii_-_input_array_is_sorted/solution.js
+++ problems/two_sum_ii_-_input_array_is_sorted/solution.js
@@ -5,17 +5,17 @@
  */
 var twoSum = function(numbers, target) {
     let l = 0,
-        r = numbers.length - 1;
+        r = numbers.length -1;
     
     while (l < r) {
         let sum = numbers[l] + numbers[r];
-        if (sum === target) {
-            return [l + 1, r + 1];
-        } else if (sum < target) {
+        if (sum < target) {
             l++;
-        } else {
+        } else if (sum > target) {
             r--;
-        }        
+        } else {
+            return [l + 1, r + 1];
+        }
     }
     
 };
