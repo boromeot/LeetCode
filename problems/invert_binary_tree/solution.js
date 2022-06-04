786a9d6bb8f62289f7f30ca1ae68e6f0b34ee74f
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-04 23:16:22 +0000

    Sync LeetCode submission - Invert Binary Tree (javascript)

--- problems/invert_binary_tree/solution.js
+++ problems/invert_binary_tree/solution.js
@@ -12,11 +12,9 @@
  */
 var invertTree = function(root) {
     if (!root) return root;
-    let tmp = root.left;
-    root.left = root.right;
-    root.right = tmp;
-    
+    [root.left, root.right] = [root.right, root.left];
     invertTree(root.left);
     invertTree(root.right);
     return root;
+    
 };
