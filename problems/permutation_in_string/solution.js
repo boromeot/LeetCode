47b5b57478f53629f8eb70d70477ad943a579b2d
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-04 03:44:49 +0000

    Sync LeetCode submission - Permutation in String (javascript)

--- problems/permutation_in_string/solution.js
+++ problems/permutation_in_string/solution.js
@@ -4,37 +4,32 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-	const map = {};
-    
-    //Count occurences of letters in s1
+    const map = {};
     for (let i = 0; i < s1.length; i++) {
-        map[s1[i]] = (map[s1[i]] || 0) + 1;
+        map[s1[i]] ? map[s1[i]]++ : map[s1[i]] = 1;
     }
-
-    //Set up pointers
+    
     let l = 0,
         r = 0;
     
-    //If reqLen === 0 return true;
+    // This number will only decrement when r encounters a valid char
+    // this number will only increment when l leave a valid char
+    // if this number is zero then the sliding window contains all the valid char needed
     let reqLen = s1.length;
     
-    //iterate the pointers till r reaches end of s2
     while (r < s2.length) {
         if (map[s2[r]] > 0) reqLen--;
-        
         map[s2[r]]--;
         r++;
-        
         if (reqLen === 0) return true;
         
+        // If this is true then the sliding window is too big and must be shortend from the left
         if (r - l === s1.length) {
             if (map[s2[l]] >= 0) reqLen++;
             map[s2[l]]++;
             l++;
         }
-        
     }
     return false;
-    
     
 };
