5c822145e6fac6ee326e9ad509c9bb060c774d66
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-05 01:57:55 +0000

    Sync LeetCode submission - Binary Tree Right Side View (javascript)

--- problems/binary_tree_right_side_view/solution.js
+++ problems/binary_tree_right_side_view/solution.js
@@ -11,21 +11,21 @@
  * @return {number[]}
  */
 var rightSideView = function(root) {
-    if (!root) return [];
-    
-    const q = [root];
     const res = [];
+    const q = [];
+    if (root) q.push(root);
     
     while (q.length > 0) {
-        const level = [];
         const qLen = q.length;
+        const level = [];
         for (let i = 0; i < qLen; i++) {
             const node = q.shift();
             level.push(node.val);
             if (node.left) q.push(node.left);
             if (node.right) q.push(node.right);
         }
-        res.push(level.at(-1));
+        res.push(level[level.length - 1]);
     }
+    
     return res;
 };
