ab04345322fa59231889bb9fa041c564f2e26e45
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-06 11:08:40 +0000

    Sync LeetCode submission - Surrounded Regions (javascript)

--- problems/surrounded_regions/solution.js
+++ problems/surrounded_regions/solution.js
@@ -3,56 +3,51 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function(board) {
-    const rLen = board.length,
-          cLen = board[0].length;
+    const ROWS = board.length,
+          COLS = board[0].length;
     
-    for (let r = 0; r < rLen; r++) {
-        if (board[r][0] === 'O') {
-            bfs(r, 0);
-        } 
-        if (board[r][cLen - 1] === 'O') {
-            bfs(r, cLen - 1);
+    for (let i = 0; i < ROWS; i++) {
+        if (board[i][0] === 'O') {
+            dfs(i, 0);
+        }
+        if (board[i][COLS - 1] === 'O') {
+            dfs(i, COLS - 1);
         }
     }
     
-    for (let c = 0; c < cLen; c++) {
-        if (board[0][c] === 'O') {
-            bfs(0, c);
+    for (let j = 0; j < COLS; j++) {
+        if (board[0][j] === 'O') {
+            dfs(0, j);
         }
-        if (board[rLen - 1][c] === 'O') {
-            bfs(rLen - 1, c);
+        if (board[ROWS - 1][j] === 'O') {
+             dfs(ROWS - 1, j);
         }
     }
-    for (let i = 0; i < rLen; i++) {
-        for (let j = 0; j < cLen; j++) {
+    
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
             if (board[i][j] === 'O') {
                 board[i][j] = 'X';
-            }
-            if (board[i][j] === 'T') {
+            } else if (board[i][j] === 'T') {
                 board[i][j] = 'O';
             }
         }
     }
     
-    function bfs(r, c) {
-        if (board[r][c] === 'T') return;
-        const q = [];
-        board[r][c] = 'T';
-        q.push([r, c]);
-        while (q.length > 0) {
-            let [row, col] = q.shift();
-            const directions = [[0, -1], [0, 1], [1, 0], [-1, 0]];
-            for (let [dr, dc] of directions) {
-                let [newR, newC] = [row + dr, col + dc];
-                if (newR < rLen && newR >= 0 &&
-                    newC < cLen && newC >= 0 &&
-                    board[newR][newC] === 'O') 
-                {
-                    q.push([newR, newC]);
-                    board[newR][newC] = 'T';
-                }
-            }
+    function dfs(r, c) {
+        if (r < 0 || r >= ROWS ||
+            c < 0 || c >= COLS ||
+            board[r][c] !== 'O') {
+            return;
         }
+        
+        board[r][c] = 'T';
+        
+        dfs(r + 1, c);
+        dfs(r - 1, c);
+        dfs(r, c + 1);
+        dfs(r, c - 1);
+        return;
     }
     
 };
