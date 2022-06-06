aa160894d84fe4f6a0069f11057dbd13c5e5206b
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-06 04:12:36 +0000

    Sync LeetCode submission - Number of Islands (javascript)

--- problems/number_of_islands/solution.js
+++ problems/number_of_islands/solution.js
@@ -3,35 +3,33 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-    const height = grid.length,
-          width = grid[0].length;
+    const ROWS = grid.length,
+          COLS = grid[0].length;
     let islands = 0;
     
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
             if (grid[i][j] === '1') {
-                bfs(i, j);
                 islands++;
+                bfs(i, j);
             }
         }
     }
     
     return islands;
-    
-    function bfs(row, col) {
-        const q = [[row, col]];
-        while (q.length > 0) {
-            let [r, c] = q.shift();
-            if (r < 0 || r >= height || c < 0 || c >= width || grid[r][c] === '0') continue;
+    function bfs(r, c) {
+        if (r < 0 || r >= ROWS || c < 0 || c >= COLS) return;
+        
+        if (grid[r][c] === '1') {
             grid[r][c] = '0';
-            for (let dir of directions) {
-                q.push([r + dir[0], c + dir[1]]);
-            }
+        } else {
+            return;
         }
         
+        bfs(r + 1, c);
+        bfs(r - 1, c);
+        bfs(r, c + 1);
+        bfs(r, c - 1);
     }
+    
 };
-// 1 1 1
-// 0 1 0
-// 1 1 1
