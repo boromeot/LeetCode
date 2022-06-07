924df3511d73948adedf32385bbc0db801f3b999
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-07 06:27:49 +0000

    Sync LeetCode submission - Redundant Connection (javascript)

--- problems/redundant_connection/solution.js
+++ problems/redundant_connection/solution.js
@@ -3,26 +3,24 @@
  * @return {number[]}
  */
 var findRedundantConnection = function(edges) {
-    const parent = [],
-          rank = [];
+    const parent = [];
+    const rank = new Array(edges.length).fill(1);
     for (let i = 0; i < edges.length + 1; i++) {
-        parent.push(i);
-        rank.push(1);
+        parent[i] = i;
     }
     
-    for (let [n1, n2] of edges) {
-        if (!union(n1, n2)) return [n1, n2];
+    for (let [v1, v2] of edges) {
+        if (!union(v1, v2)) return [v1, v2];
     }
     
-    function find(n) {
-        let p = parent[n];
-        
-        while (p != parent[p]) {
-            parent[p] = parent[parent[p]];
+    function find(i) { //Given a node i returns its represenative
+        let p = parent[i];
+        while (p !== parent[p]) {
             p = parent[p];
         }
         return p;
     }
+
     
     function union(n1, n2) {
         let p1 = find(n1),
@@ -34,10 +32,8 @@ var findRedundantConnection = function(edges) {
         
         if (rank[p1] > rank[p2]) {
             parent[p2] = p1;
-            rank[p1] += rank[p2];
         } else {
             parent[p1] = p2;
-            rank[p2] += rank[p1];
         }
         return true;
     }
