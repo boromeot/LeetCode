2e7350a02a644e9e12a48b45182fd9765cb2ca5e
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-08 04:18:10 +0000

    Sync LeetCode submission - Number of Islands (javascript)

--- problems/number_of_islands/solution.js
+++ problems/number_of_islands/solution.js
@@ -5,6 +5,7 @@
 var numIslands = function(grid) {
     const ROWS = grid.length,
           COLS = grid[0].length;
+    
     let islands = 0;
     
     for (let i = 0; i < ROWS; i++) {
@@ -15,21 +16,41 @@ var numIslands = function(grid) {
             }
         }
     }
-    
+   
     return islands;
+    
     function bfs(r, c) {
-        if (r < 0 || r >= ROWS || c < 0 || c >= COLS) return;
+        const DIRECTIONS = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+        const q = [[r, c]];
         
-        if (grid[r][c] === '1') {
-            grid[r][c] = '0';
-        } else {
-            return;
+        while (q.length > 0) {
+            const qLen = q.length;
+            for (let i = 0; i < qLen; i++) {
+                const [row, col] = q.shift();
+                for (let [dr, dc] of DIRECTIONS) {
+                    const [adjR, adjC] = [row + dr, col + dc];
+                    if (adjR >= 0 && adjR < ROWS &&
+                        adjC >= 0 && adjC < COLS &&
+                        grid[adjR][adjC] === '1') {
+                        grid[adjR][adjC] = '0';
+                        q.push([adjR, adjC]);
+                    }
+                }
+            }
         }
+    }
+    
+    function dfs(r, c) {
+        if (r < 0 || r >= ROWS ||
+            c < 0 || c >= COLS ||
+            grid[r][c] === '0')  return;
         
-        bfs(r + 1, c);
-        bfs(r - 1, c);
-        bfs(r, c + 1);
-        bfs(r, c - 1);
+        grid[r][c] = '0';
+        
+        dfs(r + 1, c);
+        dfs(r - 1, c);
+        dfs(r, c + 1);
+        dfs(r, c - 1);
     }
     
 };
