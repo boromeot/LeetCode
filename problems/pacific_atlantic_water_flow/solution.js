4d4d366b43529acecf378ddeb07ac2fb67763ecc
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-08 04:49:11 +0000

    Sync LeetCode submission - Pacific Atlantic Water Flow (javascript)

--- problems/pacific_atlantic_water_flow/solution.js
+++ problems/pacific_atlantic_water_flow/solution.js
@@ -5,48 +5,46 @@
 var pacificAtlantic = function(heights) {
     const ROWS = heights.length,
           COLS = heights[0].length;
-    const pacificSet = new Set(),
-          atlanticSet = new Set(); // These sets hold the points that reach each, respectievly
-        
+    
+    const pacificSet = new Set();
+    const atlanticSet = new Set();
+    
     for (let i = 0; i < ROWS; i++) {
-        dfs(i, 0, pacificSet, heights[i][0] );
-        dfs(i, COLS - 1, atlanticSet, heights[i][COLS - 1]);
+        dfs(i, 0, pacificSet, -1);
+        dfs(i, COLS - 1, atlanticSet, -1);
     }
     
     for (let j = 0; j < COLS; j++) {
-        dfs(0, j, pacificSet, heights[0][j]);
-        dfs(ROWS - 1, j, atlanticSet, heights[ROWS - 1][j]);
+        dfs(0, j, pacificSet, -1);
+        dfs(ROWS - 1, j, atlanticSet, -1);
     }
     
     const res = [];
-    
-    for (let k = 0; k < ROWS; k++) {
-        for (let l = 0; l < COLS; l++) {
-            if (pacificSet.has(k + ',' + l) && atlanticSet.has(k + ',' + l)) {
-                res.push([k, l]);
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
+            if (pacificSet.has(i + ',' + j) && atlanticSet.has(i + ','+ j)) {
+                res.push([i, j]);
             }
         }
     }
-    
     return res;
     
     function dfs(r, c, set, prevHeight) {
-
+        const DIRECTIONS = [[1, 0], [-1, 0], [0, 1], [0, -1]];
         if (r < 0 || r >= ROWS ||
-            c < 0 || c >= COLS ||
+            c < 0 || c >= COLS || 
             set.has(r + ',' + c) ||
-            heights[r][c] < prevHeight) 
-        {
-            return 
+            heights[r][c] < prevHeight) {
+            return;
         }
-        
-        const currHeight = heights[r][c];
-        
         set.add(r + ',' + c);
+        const currHeight = heights[r][c];
         
         dfs(r + 1, c, set, currHeight);
         dfs(r - 1, c, set, currHeight);
         dfs(r, c + 1, set, currHeight);
         dfs(r, c - 1, set, currHeight);
+        
     }
+    
 };
