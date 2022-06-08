de267526bc82516b401e800a641a8e1076a67d3e
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-08 08:06:13 +0000

    Sync LeetCode submission - Redundant Connection (javascript)

--- problems/redundant_connection/solution.js
+++ problems/redundant_connection/solution.js
@@ -5,36 +5,38 @@
 var findRedundantConnection = function(edges) {
     const parent = [];
     const rank = new Array(edges.length).fill(1);
-    for (let i = 0; i < edges.length + 1; i++) {
+    
+    for (let i = 0; i < edges.length; i++) {
         parent[i] = i;
     }
     
-    for (let [v1, v2] of edges) {
-        if (!union(v1, v2)) return [v1, v2];
-    }
-    
-    function find(i) { //Given a node i returns its represenative
-        let p = parent[i];
-        while (p !== parent[p]) {
-            p = parent[p];
+    function find(node) {
+        let currParent = parent[node];
+        while (currParent !== parent[currParent]) {
+            parent[currParent] = parent[parent[currParent]]; // PATH compression
+            currParent = parent[currParent];
         }
-        return p;
+        return currParent;
     }
-
     
+    for (let [n1, n2] of edges) {
+        if (!union(n1, n2)) return [n1, n2];
+    }
+     
     function union(n1, n2) {
         let p1 = find(n1),
             p2 = find(n2);
         
-        if (p1 === p2) {
-            return false;
-        }
+        if (p1 === p2) return false; //Unable to unify
         
         if (rank[p1] > rank[p2]) {
             parent[p2] = p1;
+            rank[p1] += rank[p2];
         } else {
             parent[p1] = p2;
+            rank[p2] += rank[p1];
         }
-        return true;
+        
+        return true; //Was able to unify
     }
 };
