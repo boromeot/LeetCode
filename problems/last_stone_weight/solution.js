0594d49d0d738ac9a64b9a85d364294eba5ea5fd
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-10 14:19:30 +0000

    Sync LeetCode submission - Last Stone Weight (javascript)

--- problems/last_stone_weight/solution.js
+++ problems/last_stone_weight/solution.js
@@ -2,101 +2,93 @@
  * @param {number[]} stones
  * @return {number}
  */
-
-/*
-[2,7,4,1,8,1]
-   8
- 7   4
-1 2 3
-*/
 var lastStoneWeight = function(stones) {
-    const heap = new maxHeap(stones);
+
+    const heap = new MaxHeap(stones);
+    // console.log(heap)
+
     while (heap.size() > 1) {
-        const max1 = heap.poll();
-        const max2 = heap.poll();
-        if (max1 > max2) heap.insert(max1 - max2);
+        let s1 = heap.poll(),
+            s2 = heap.poll();
+        
+        heap.insert(Math.abs(s1 - s2));
     }
-    return heap.size() === 1 ? heap.peek() : 0;
+    
+    if (heap.size() === 1) return heap.poll();
+    return 0;
     
 };
 
-class maxHeap {
+class   MaxHeap {
     constructor(data = []) {
         this.data = data;
         this.heapify();
     }
     
     heapify() {
-        if (this.size() <= 1) return;
+        if (this.size <= 1) return;
         for (let i = this.size() - 1; i >= 0; i--) {
             this.bubbleDown(i);
         }
-    }
-    
-    peek() {
-        if (this.size() === 0) return null;
-        return this.data[0];
+        
     }
     
     poll() {
-        if (this.size() === 0) return null;
-        const result = this.data[0];
+        if (this.size() <= 0) return null;
+        const res = this.data[0];
         const last = this.data.pop();
-        if (this.size() !== 0) {
+        if (this.size() > 0) {
             this.data[0] = last;
             this.bubbleDown(0);
         }
-        return result;
+        return res;
+    }
+    
+    bubbleDown(i) {
+        const n = this.size();
+        let mindex = i;
+        
+        while (i < n) {
+            let l = 2 * i + 1,
+                r = 2 * i + 2;
+            
+            if (l < n && this.data[l] > this.data[i] || 
+                r < n && this.data[r] > this.data[i]) {
+                if (r < n) {
+                    mindex = this.data[l] > this.data[r] ? l : r;
+                } else {
+                    mindex = l;
+                }
+            }
+            if (mindex === i) break;
+            this.swap(mindex, i);
+            i = mindex;
+        }
     }
     
     insert(val) {
         this.data.push(val);
-        let lastIndex = this.size() - 1;
-        this.bubbleUp(lastIndex);
+        this.bubbleUp(this.size() - 1);
     }
-    
+
     bubbleUp(i) {
-        while (i > 0) {
-            const parentIndex = Math.floor((i - 1) / 2);
-            if (this.data[i] > this.data[parentIndex] ) {
-                this.swap(parentIndex, i);
-                i = parentIndex;
+        while(i > 0) {
+            let pIndex = Math.floor((i - 1) / 2);
+            
+            if (this.data[i] > this.data[pIndex]) {
+                this.swap(i, pIndex);
+                i = pIndex;
             } else {
                 break;
             }
         }
     }
     
-    bubbleDown(i) {
-        let max = i;
-        const n = this.size();
-        
-        while (i < n) {
-            let l = i * 2 + 1;
-            let r = l + 1;
-            
-            if (l < n && this.data[l] > this.data[i] ||
-                r < n && this.data[r] > this.data[i]) {
-                if (r < n) {
-                    max = this.data[l] > this.data[r]  ? l : r;
-                } else {
-                    max = l;
-                }
-            }
-            
-            if (max === i) break;
-            this.swap(i, max);
-            i = max;
-        }
-        
-    }
-    
     swap(i, j) {
-        [this.data[i], this.data[j]] = [this.data[j], this.data[i]];
+            [this.data[i], this.data[j]] = [this.data[j], this.data[i]];
     }
-    
+        
     size() {
         return this.data.length;
     }
-    
 }
