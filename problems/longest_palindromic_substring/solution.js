08657fbc7a2d015645996ff2f88781de7cadc3fe
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-13 03:18:06 +0000

    Sync LeetCode submission - Longest Palindromic Substring (javascript)

--- problems/longest_palindromic_substring/solution.js
+++ problems/longest_palindromic_substring/solution.js
@@ -3,36 +3,35 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    let maxLen = 0,
-        minL = 0,
-        maxR = 0;
-    
+
+    let minL = 0,
+        maxR = 0,
+        maxLen = 0;
     
     for (let i = 0; i < s.length; i++) {
-        
-        //Even case
         let l = i,
             r = i;
         
         while (l >= 0 && r < s.length && s[l] === s[r]) {
-            if (r - l + 1 > maxLen) {
+            let curLen = r - l + 1;
+            if (curLen > maxLen) {
+                maxLen = curLen;
                 minL = l;
                 maxR = r;
-                maxLen = r - l + 1;
             }
             l--;
             r++;
         }
         
-        //Odd case
-        l = i;
+        l = i,
         r = i + 1;
         
         while (l >= 0 && r < s.length && s[l] === s[r]) {
-            if (r - l + 1 > maxLen) {
+            let curLen = r - l + 1;
+            if (curLen > maxLen) {
+                maxLen = curLen;
                 minL = l;
                 maxR = r;
-                maxLen = r - l + 1;
             }
             l--;
             r++;
