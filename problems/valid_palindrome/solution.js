d569b6b08aa2bac17d1910ae01f987557c10ccbf
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-14 14:51:06 +0000

    Sync LeetCode submission - Valid Palindrome (javascript)

--- problems/valid_palindrome/solution.js
+++ problems/valid_palindrome/solution.js
@@ -3,30 +3,23 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    let set = new Set('abcdefghijklmnopqrstuvwxyz0123456789'.split(''));
+
     let l = 0,
         r = s.length - 1;
     
+    s = s.toLowerCase();
+    
     while (l < r) {
-        if (!isAlphaNum(s[l])) {
-            l++;
-            continue;
-        } else if (!isAlphaNum(s[r])) {
-            r--;
-        } else {
-            if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
-            l++;
-            r--;
-        }
+       if (!set.has(s[l])) {
+           l++;
+       } else if (!set.has(s[r])) {
+           r--;
+       } else {
+           if (s[l] !== s[r]) return false;
+           l++;
+           r--;
+       }
     }
     return true;
-}
-
-function isAlphaNum(c) {
-    return (
-        'A'.charCodeAt() <= c.charCodeAt() && c.charCodeAt() <= 'Z'.charCodeAt() ||
-        'a'.charCodeAt() <= c.charCodeAt() && c.charCodeAt() <= 'z'.charCodeAt() ||
-        '0'.charCodeAt() <= c.charCodeAt() && c.charCodeAt() <= '9'.charCodeAt()
-    )
-}
-// 4004
-// 404
+};
