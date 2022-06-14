66e9935235a5e70c21845dd457e0b4df79482c1d
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-14 12:03:00 +0000

    Sync LeetCode submission - Valid Anagram (javascript)

--- problems/valid_anagram/solution.js
+++ problems/valid_anagram/solution.js
@@ -4,25 +4,24 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length) {
-        return false;
-    }
+    if (s.length !== t.length) return false;
     
-    const map1 = new Map();
+    const map = {};
     
-    for (let i = 0; i < s.length; i++) {
-        const c = s[i];
-        if (!map1.has(c)) {
-            map1.set(c, 1);
+    for (let c of s) {
+        if (map[c] !== undefined) {
+          map[c]++;
         } else {
-            map1.set(c, map1.get(c) + 1);
+            map[c] = 1;
         }
     }
     
-    for (i = 0; i < t.length; i++) {
-        const c = t[i];
-        if (!map1.get(c)) return false;
-        map1.set(c, map1.get(c) - 1);
+    let reqLen = s.length;
+    
+    for (let c of t) {
+        if (map[c]) reqLen--; // Everytime it encounters a valid char the reqLen decreases
+        map[c]--;
+        if (reqLen === 0) return true;
     }
-    return true;
+    return false;
 };
