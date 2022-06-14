3f17e5ca639475bec99ba9ceb3e9a22e30099ab2
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-14 23:58:33 +0000

    Sync LeetCode submission - 3Sum (javascript)

--- problems/3sum/solution.js
+++ problems/3sum/solution.js
@@ -5,17 +5,16 @@
 var threeSum = function(nums) {
     const target = 0;
     const res = [];
-    if (nums.length < 3) return res;
-    
     nums.sort((a, b) => a - b);
     
+    // [-4,-1,-1,-1,0,1,2]
+    //      i  l        r
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > target) break;
+        if (nums[i] > 0) break;
         
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let l = i + 1,
+            r = nums.length - 1;
         
-        let l = i + 1;
-        let r = nums.length - 1;
         while (l < r) {
             let sum = nums[i] + nums[l] + nums[r];
             if (sum < target) {
@@ -24,15 +23,28 @@ var threeSum = function(nums) {
                 r--;
             } else {
                 res.push([nums[i], nums[l], nums[r]]);
-                while(nums[l] === nums[l + 1]) l++;
-                while(nums[r] === nums[r - 1]) r--;
+                while (nums[l] === nums[l + 1] && l < r) {
+                    l++;
+                }
+                while (nums[r] === nums[r - 1] && r > l) {
+                    r--;
+                }
                 l++;
                 r--;
             }
         }
+        while (nums[i] === nums[i + 1] && i < nums.length) {
+            i++;
+        }
+        
     }
     return res;
-};
+    // [0]
+    //  i  l  r
+    
 
-//[-1,0,1,-1,-2,2,-1,-4]
-//[-4,-2,-1,-1,-1,0,1,2,2]
+    
+    // a + b + c = 0
+    // b + c = -a
+    
+};
