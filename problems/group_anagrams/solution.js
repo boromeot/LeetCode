2307baa931d56062f4ac1ecfc52713e593cd90fd
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-14 12:38:36 +0000

    Sync LeetCode submission - Group Anagrams (javascript)

--- problems/group_anagrams/solution.js
+++ problems/group_anagrams/solution.js
@@ -3,28 +3,18 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
+    const map = {} // { anagramKey: [anagram1, anagram2] }
 
-    const res = [];
-    const map = {};
-    
-    for (let i = 0; i < strs.length; i++) {
-        const count = new Array(26).fill(0);
-        const s = strs[i];
-        
-        for (let j = 0; j < s.length; j++) {
-            const c = s[j];
-            count[c.charCodeAt() - 97]++;
+    for (let str of strs) {
+        const buckets = new Array(26).fill(0); // [0, 0, 0, 0...]
+        for (let c of str) {
+            let cIndex = c.charCodeAt(0) - 97; // a.charCodeAt(0) - 97 :=>: 97 - 97
+            buckets[cIndex]++;
         }
-        
-        const key = count.join(':)');
-        
-        if (map[key]) {
-            map[key].push(s);
-        } else {
-            map[key] = [s];
-        }
+        const anagramKey = buckets.join(',');
+        map[anagramKey] ? map[anagramKey].push(str) : map[anagramKey] = [str];
     }
-    
-    return Object.values(map);
-    
+    return Object.values(map); // [[str1, str2], [str3], [str4, str5, str6]]
 };
+
+// Anagrams have distinct keys, 
