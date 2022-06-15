64c301dcb3afbd902509f2e1ccefb37fbbc3cd74
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-15 20:54:36 +0000

    Sync LeetCode submission - Koko Eating Bananas (javascript)

--- problems/koko_eating_bananas/solution.js
+++ problems/koko_eating_bananas/solution.js
@@ -4,21 +4,43 @@
  * @return {number}
  */
 var minEatingSpeed = function(piles, h) {
-    let l = 1,
-        r = Math.max(...piles);
-    let res = r;
+    let l = 1;
+    let r = Math.max(...piles); // O(n)
+    
+    let k = r;
+    
     while (l <= r) {
-        let m = Math.trunc((l + r) / 2);
-        let t = 0;
-        for (let i = 0; i < piles.length; i++) {
-            t += Math.ceil(piles[i] / m);
+        let eatSpeed = Math.floor((l + r) / 2);
+        
+        let time = 0;
+        for (let pile of piles) {
+            time += Math.ceil(pile / eatSpeed);
         }
-        if (t <= h) {
-            res = Math.min(res, m);
-            r = m - 1;
-        } else {
-            l = m + 1;
+        
+        if (time <= h) {
+            k = Math.min(k, eatSpeed);
+            r = eatSpeed - 1;
+        } else if (time > h) {
+            l = eatSpeed + 1;
         }
     }
-    return res;
+    return k;
 };
+
+/*
+[3,6,7,11] h = 8, k = 6
+ 1 2 2 3
+time it takes to eat a pile = Math.ceil(pileSize / eatSpeed)
+
+times it taktes to eat all piles (l) = Σ Math.ceil(piles[i] / eatSpeed) from i = 0 to i = piles.length - 1;
+ 
+ if l > h : eatSpeed 
+ 
+ 1 2 3 4 5 6 7 8 9 10 11
+       l r
+       e
+ h = 1 -> 2 -> 3 -> 4 -> 5
+ 
+search area = 0 <--> 11 
+              0 <--> Math.max(...piles)
+*/
