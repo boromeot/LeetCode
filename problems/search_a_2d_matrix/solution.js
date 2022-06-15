18db039159e3a146a884fd274bd95c394f3d4700
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-15 20:17:09 +0000

    Sync LeetCode submission - Search a 2D Matrix (javascript)

--- problems/search_a_2d_matrix/solution.js
+++ problems/search_a_2d_matrix/solution.js
@@ -2,38 +2,52 @@
  * @param {number[][]} matrix
  * @param {number} target
  * @return {boolean}
- */
+ *//* T = 13
+[ 1, 3, 5, 7] 0
+[10,11,16,20] 1 m
+[23,30,34,60] 2
+  0  1  2  3
+*/
 var searchMatrix = function(matrix, target) {
-    const rows = matrix.length - 1,
-          cols = matrix[0].length - 1;
-    
-    let t = 0,
-        b = rows,
-        c;
-    while (t <= b) {
-        c = Math.trunc((t + b) / 2);
-        if (matrix[c][0] <= target && target <= matrix[c][cols]) break;
+    const ROWS = matrix.length,
+          COLS = matrix[0].length;
+    //First find which row its in
+    let rowL = 0,
+        rowR = ROWS - 1;
+    let targetRow;
+    while (rowL <= rowR) {
+        let rowM = Math.floor((rowL + rowR) / 2);
         
-        if (target < matrix[c][0]) {
-            b = c - 1;
-        } else if (matrix[c][cols] < target) {
-            t = c + 1;
+        let leftNum = matrix[rowM][0];
+        let rightNum = matrix[rowM][COLS - 1];
+        
+        if (leftNum <= target && target <= rightNum) {
+            targetRow = rowM;
+            break;
+        } else if (target < leftNum) {
+            rowR = rowM - 1;
+        } else if (target > rightNum) {
+            rowL = rowM + 1;
         }
     }
     
+    if (targetRow === undefined) return false;
+    
     let l = 0,
-        r = cols;
+        r = COLS - 1;
     
     while (l <= r) {
-        let m = Math.trunc((l + r) / 2);
-        
-        if (matrix[c][m] === target) return true;
-        
-        if (matrix[c][m] < target) {
+        let m = Math.floor((r + l) / 2);
+        let num = matrix[targetRow][m];
+        if (num < target) {
             l = m + 1;
-        } else {
+        } else if (num > target) {
             r = m - 1;
+        } else {
+            return true;
         }
     }
     return false;
 };
+
+
