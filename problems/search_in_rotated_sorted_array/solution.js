f0d4a2fa23fdc0338d4cce1e7bb76de6f9feb0c2
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-15 21:25:06 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (javascript)

--- problems/search_in_rotated_sorted_array/solution.js
+++ problems/search_in_rotated_sorted_array/solution.js
@@ -4,28 +4,40 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let l = 0,
+    let l = 0, 
         r = nums.length - 1;
     
     while (l <= r) {
-        let m = Math.trunc((l + r) / 2);
+        let m = Math.floor((l + r) / 2);
+        
         if (nums[m] === target) return m;
         
-        if (nums[l] <= nums[m]) {
-            if (nums[l] <= target && target <= nums[m]) {
+        if (nums[l] <= nums[m]) { //Window is sorted from l -> m
+            if (nums[l] <= target && target <= nums[m]) { // Target is within nums[l] -> nums[m]
                 r = m - 1;
-            } else {
-                l = m + 1;             
-            }
-        } else {
-            if (nums[m] <= target && target <= nums[r]) {
+            } else { // Target is within nums[m] -> nums[r]
                 l = m + 1;
-            } else {
+            }
+            
+        } else { //Window is sorted from m -> r
+            if (nums[m] <= target && target <= nums[r]) { // Target is within num[m] -> nums[r]
+                l = m + 1;
+            } else { // Target is within nums[l] -> nums[m]
                 r = m - 1;
             }
         }
     }
     return -1;
-}; // [4,5,6,7,0,1,2] 1
-//     l     m t   r
-//           l m
+};
+
+/*
+[4,5,6,7,0,1,2]
+ l     m     r
+if (nums[l] <= nums[r]) nums[l] ---> nums[r] is sorted
+
+else if (nums[l] > nums[r]) { nums[l] ---> nums[r] is not sorted
+    
+
+} 
+
+*/
