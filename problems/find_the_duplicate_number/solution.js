1f3fb295f4d62806f56c27f6b939acd1f99f7797
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-16 14:22:09 +0000

    Sync LeetCode submission - Find the Duplicate Number (javascript)

--- problems/find_the_duplicate_number/solution.js
+++ problems/find_the_duplicate_number/solution.js
@@ -2,13 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findDuplicate = function(nums) {
+var findDuplicate = function(nums) { // O(n^2), O(1)
+    
     let slow = 0,
         fast = 0;
-        
+    
     while (true) {
         slow = nums[slow];
         fast = nums[nums[fast]];
+        
         if (slow === fast) break;
     }
     
@@ -16,7 +18,16 @@ var findDuplicate = function(nums) {
     
     while (true) {
         slow = nums[slow];
-        slow2 = nums[slow2];
+        slow2= nums[slow2];
         if (slow === slow2) return slow;
     }
+    
 };
+
+// constant space almost always means pointers!! ! ! ! yay
+/*
+
+[1,2,3,4,2]
+ l       r
+
+*/
