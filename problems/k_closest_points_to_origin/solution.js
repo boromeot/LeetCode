b833c08eb6925ab4ffeb979b51c4441f9c83cd03
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-18 23:20:33 +0000

    Sync LeetCode submission - K Closest Points to Origin (javascript)

--- problems/k_closest_points_to_origin/solution.js
+++ problems/k_closest_points_to_origin/solution.js
@@ -4,31 +4,30 @@
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
+    // Create min heap
     const heapItems = [];
-    for (let i = 0; i < points.length; i++) {
-        const point = points[i];
-        const priority = distance(point[0], point[1]);
-        heapItems.push(new HeapItem(point, priority));
+    for (let [x, y] of points) {
+        let distance = distanceFromOrigin(x, y);
+    
+        heapItems.push(new HeapItem(distance, [x, y]));
     }
-
     const heap = new MinHeap(heapItems);
-    const res = [];
-    
-    for (i = 0; i < k; i++) {
-        res.push(heap.poll().item);
+    // Poll heap k times and return the kth poll
+    let res = [];
+    for (let i = 0; i < k; i++) {
+        res.push(heap.poll().point);
     }
-    
     return res;
-    
-    function distance(x1, y1, x2 = 0, y2 = 0) {
-        return Math.pow(x1, 2) + Math.pow(y1, 2);
-    }
+};
+
+function distanceFromOrigin(x, y) { // Distance from origin
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 }
 
 class HeapItem {
-    constructor(item, priority = item) {
-        this.item = item;
+    constructor(priority, point) {
         this.priority = priority;
+        this.point = point;
     }
 }
 
@@ -39,66 +38,43 @@ class MinHeap {
     }
     
     heapify() {
-        for (let i = 1; i < this.size(); i++) {
-            this.bubbleUp(i);
-        }
-    }
-    
-    peek(i) {
-        if (this.size === 0) return null;
-        return this.data[i];
-    }
-    
-    poll() {
-        if (this.size() === 0) return null;
-        const result = this.data[0];
-        const last = this.data.pop();        
-        if (this.size() !== 0) {
-            this.data[0] = last;
-            this.bubbleDown(0);
-        }
-        return result;
-    }
-    
-    insert(val) {
-        this.data.push(val);
-        const lastIndex = this.size() - 1;
-        this.bubbleUp(lastIndex);
-    }
-    
-    bubbleUp(i) {
-        while (i > 0) {
-            let parentIndex = Math.floor((i - 1) / 2);
-            if (this.data[i].priority < this.data[parentIndex].priority) {
-                this.swap(i, parentIndex);
-                i = parentIndex
-            } else {
-                break;
-            }
+        if (this.size() <= 1) return;
+        for (let i = this.size() - 1; i >= 0; i--) {
+            this.bubbleDown(i);
         }
     }
     
     bubbleDown(i) {
-        let minIndex = i;
         const n = this.size();
+        let mindex = i;
         
         while (i < n) {
-            let l = 2 * i + 1;
-            let r = 2 * i + 2;
+            let l = 2 * i + 1,
+                r = 2 * i + 2;
             
-            if (l < n && this.data[l].priority < this.data[i].priority || 
+            if (l < n && this.data[l].priority < this.data[i].priority ||
                 r < n && this.data[r].priority < this.data[i].priority) {
                 if (r < n) {
-                    minIndex = this.data[l].priority < this.data[r].priority ? l : r;
+                    mindex = this.data[l].priority < this.data[r].priority ? l : r;
                 } else {
-                    minIndex = l;
+                    mindex = l;
                 }
             }
-            
-            if (minIndex === i) break;
-            this.swap(minIndex, i);
-            i = minIndex;
+            if (i === mindex) break;
+            this.swap(i, mindex);
+            i = mindex;
         }
+    }
+    
+    poll() {
+        if (this.size() <= 0) return null;
+        let min = this.data[0];
+        let last = this.data.pop();
+        if (this.size() > 0) {
+            this.data[0] = last;
+            this.bubbleDown(0);
+        }
+        return min;
     }
     
     swap(i, j) {
