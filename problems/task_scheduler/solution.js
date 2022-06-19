c2be85bc4702afb57b39367b407f15e3ee18da3b
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-19 02:52:51 +0000

    Sync LeetCode submission - Task Scheduler (javascript)

--- problems/task_scheduler/solution.js
+++ problems/task_scheduler/solution.js
@@ -2,46 +2,50 @@
  * @param {character[]} tasks
  * @param {number} n
  * @return {number}
-   A
-  A A
- 
  */
 var leastInterval = function(tasks, n) {
     const map = {};
-    for (let i = 0; i < tasks.length; i++) {
-        if (!map[tasks[i]]) {
-            map[tasks[i]] = 1;
+    for (let c of tasks) {
+        if (map[c]) {
+            map[c]++;
         } else {
-            map[tasks[i]]++;
+            map[c] = 1;
         }
     }
-    const heap = new MaxHeap(Object.values(map));
-    const q = [];
-    let time = 0;
     
-    //q [[freq, timeToBeReleased]]
+    const heapNodes = [];
+    for (let [task, count] of Object.entries(map)) {
+        heapNodes.push(new HeapNode(count, task, 0));
+    }
+    
+    const q = [];
+    const heap = new MaxHeap(heapNodes);
+    let t = 0;
     while (heap.size() > 0 || q.length > 0) {
-        time++;
+        t++;
         if (heap.size() > 0) {
-            const task = heap.poll() - 1;
-            if (task) {
-                q.push([task, time + n]);        
+            let task = heap.poll();
+            task.priority--;
+            task.time = t + n;
+            if (task.priority > 0) {
+                q.push(task)
             }
         }
         
-        if (q.length > 0 && q[0][1] === time) {
+        if (q.length > 0 && q[0].time === t) {
             const task = q.shift();
-            heap.insert(task[0]);
+            heap.insert(task);
         }
+        
     }
-    return time;
-    
+    return t;
 };
 
-class HeapItem {
-    constructor(data, priority) {
-        this.data = data;
-        this.priority = priority
+class HeapNode {
+    constructor(priority, val, time = 0) {
+        this.priority = priority;
+        this.val = val;
+        this.time = time;
     }
 }
 
@@ -59,50 +63,48 @@ class MaxHeap {
     }
     
     poll() {
-        if (this.size() <= 0) {
-            return null;
-        }
-        const result = this.data[0];
-        const last = this.data.pop();
-        if (this.size() !== 0) {
+        if (this.size() <= 0) return null;
+        let max = this.data[0];
+        let last = this.data.pop();
+        if (this.size() > 0) {
             this.data[0] = last;
             this.bubbleDown(0);
         }
-        return result;
-    }
-    
-    insert(val) {
-        this.data.push(val);
-        this.bubbleUp(this.size() - 1);
+        return max;
     }
     
     bubbleDown(i) {
-        let max = i;
         const n = this.size();
+        let maxdex = i;
         
         while (i < n) {
-            let l = 2 * i + 1;
-            let r = 2 * i + 2;
+            let l = 2 * i + 1,
+                r = 2 * i + 2;
             
-            if (l < n && this.data[l] > this.data[i] ||
-                r < n && this.data[r] > this.data[i]) {
+            if (l < n && this.data[l].priority > this.data[i].priority ||
+                r < n && this.data[r].priority > this.data[i].priority) {
                 if (r < n) {
-                    max = this.data[l] > this.data[r] ? l : r;
+                    maxdex = this.data[l].priority > this.data[r].priority ? l : r;
                 } else {
-                    max = l;                    
+                    maxdex = l;
                 }
             }
-            
-            if (max === i) break;
-            this.swap(max, i);
-            i = max;
+            if (i === maxdex) break;
+            this.swap(i, maxdex);
+            i = maxdex;
         }
+    }
+    
+    insert(heapNode) {
+        this.data.push(heapNode);
+        this.bubbleUp(this.size() - 1);
     }
     
     bubbleUp(i) {
         while (i > 0) {
             let p = Math.floor((i - 1) / 2);
-            if (this.data[i] > this.data[p]) {
+            
+            if (this.data[i].priority > this.data[p].priority) {
                 this.swap(i, p);
                 i = p;
             } else {
@@ -111,15 +113,16 @@ class MaxHeap {
         }
     }
     
-    peek() {
-        return this.data[0];
-    }
-    
     swap(i, j) {
         [this.data[i], this.data[j]] = [this.data[j], this.data[i]];
+    }
+    
+    peek() {
+        return this.data[0];
     }
     
     size() {
         return this.data.length;
     }
+    
 }
