45ab6647fe2e52bff6cb3c8e775bbe205863477f
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-20 19:47:26 +0000

    Sync LeetCode submission - Walls and Gates (javascript)

--- problems/walls_and_gates/solution.js
+++ problems/walls_and_gates/solution.js
@@ -3,12 +3,12 @@
  * @return {void} Do not return anything, modify rooms in-place instead.
  */
 var wallsAndGates = function(rooms) {
-    const DIRECTIONS = [[1, 0], [-1, 0], [0, 1], [0, -1]];
     const ROWS = rooms.length,
           COLS = rooms[0].length;
-
+    const DIRECTIONS = [[1, 0],[-1, 0], [0, 1], [0, -1]];
     const q = [];
-    // O(N)
+    
+    // O(n * m)
     for (let i = 0; i < ROWS; i++) {
         for (let j = 0; j < COLS; j++) {
             if (rooms[i][j] === 0) {
@@ -21,22 +21,24 @@ var wallsAndGates = function(rooms) {
     
     function bfs() {
         let distance = 0;
-        while (q.length > 0) {
+        while (q.length) {
             const qLen = q.length;
             distance++;
             for (let i = 0; i < qLen; i++) {
-                const [r, c] = q.shift();
+                const [row, col] = q.shift();
+                
                 for (let [dr, dc] of DIRECTIONS) {
-                    const [adjRow, adjCol] = [r + dr, c + dc];
-                    //Verifiy inputs
-                    if (adjRow >= 0 && adjRow < ROWS &&
-                        adjCol >= 0 && adjCol < COLS &&
-                        rooms[adjRow][adjCol] === 2147483647) {
-                        rooms[adjRow][adjCol] = distance;
-                        q.push([adjRow, adjCol]);
+                    const [adjR, adjC] = [row + dr, col + dc];
+                    
+                    if (adjR < ROWS && adjR >= 0 &&
+                        adjC < COLS && adjC >= 0 &&
+                        rooms[adjR][adjC] === 2147483647) {
+                        rooms[adjR][adjC] = distance;
+                        q.push([adjR, adjC]);
                     }
                 }
             }
         }
     }
+    
 };
