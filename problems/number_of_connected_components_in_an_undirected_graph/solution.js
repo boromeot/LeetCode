e5ec1df1f102edf80d30c3119819c9aa54b59a53
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-20 21:18:58 +0000

    Sync LeetCode submission - Number of Connected Components in an Undirected Graph (javascript)

--- problems/number_of_connected_components_in_an_undirected_graph/solution.js
+++ problems/number_of_connected_components_in_an_undirected_graph/solution.js
@@ -4,48 +4,47 @@
  * @return {number}
  */
 var countComponents = function(n, edges) {
-    const parent = new Map(); //Given node, returns the panret of the node
-    const rank = new Map(); //Given node, returns the amount of nodes in component
+    let components = n;
+    
+    let parent = new Map();
+    let rank = new Map();
     
     for (let i = 0; i < n; i++) {
         parent.set(i, i);
-    }
-    
-    for (let i = 0; i < n; i++) {
         rank.set(i, 1);
     }
     
-    let components = n;
-    
-    for (let [n1, n2] of edges) {
-        if (union(n1, n2)) components--;
+    for (let [v1, v2] of edges) {
+        if (union(v1, v2)) {
+            components--;
+        }
     }
     
     return components;
     
-    function find(node) { //Given node, Returns the graphs root
+    function find(node) { // Returns the representitive of the group
         let p = parent.get(node);
         
         while (p !== parent.get(p)) {
-            parent.set(p, parent.get(parent.get(p))); //Path compression 
+            parent.set(p, parent.get(parent.get(p))); // Path compression
             p = parent.get(p);
         }
         return p;
     }
     
     function union(n1, n2) {
-        const p1 = find(n1),
-              p2 = find(n2);
+        let p1 = find(n1),
+            p2 = find(n2);
         
-        if (p1 === p2) return false; //Unification was unsuccessful because p1 and p2 are already unified
-        
+        if (p1 === p2) return false; // Unionfication is not possible because p1 and p2 are already unioned
+    
         if (rank.get(p1) > rank.get(p2)) {
             parent.set(p2, p1);
             rank.set(p1, rank.get(p1) + rank.get(p2));
         } else {
             parent.set(p1, p2);
-            rank.set(p2, rank.get(p1) + rank.get(p2));
+            rank.get(p2, rank.get(p1) + rank.get(p2));
         }
-        return true; //Unifcation successful
+        return true;
     }
 };
