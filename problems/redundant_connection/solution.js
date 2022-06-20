f7d07eae413e09b292e4854f6a0e7c626a0d41f2
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-06-20 20:41:51 +0000

    Sync LeetCode submission - Redundant Connection (javascript)

--- problems/redundant_connection/solution.js
+++ problems/redundant_connection/solution.js
@@ -3,31 +3,32 @@
  * @return {number[]}
  */
 var findRedundantConnection = function(edges) {
-    const parent = [];
-    const rank = new Array(edges.length).fill(1);
+    let parent = []
+    let rank = new Array(edges.length).fill(1);
     
-    for (let i = 0; i < edges.length; i++) {
+    for (let i = 1; i < edges.length + 1; i++) {
         parent[i] = i;
     }
     
-    function find(node) {
-        let currParent = parent[node];
-        while (currParent !== parent[currParent]) {
-            parent[currParent] = parent[parent[currParent]]; // PATH compression
-            currParent = parent[currParent];
-        }
-        return currParent;
+    for (let [v1, v2] of edges) {
+        if (!union(v1, v2)) return [v1, v2]
     }
     
-    for (let [n1, n2] of edges) {
-        if (!union(n1, n2)) return [n1, n2];
+    function find(i) { // Finds the representative of a node cluster
+        let p = parent[i];
+        
+        while (p !== parent[p]) { // While p is not its own parent
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
     }
-     
+    
     function union(n1, n2) {
         let p1 = find(n1),
             p2 = find(n2);
         
-        if (p1 === p2) return false; //Unable to unify
+        if (p1 === p2) return false; // Union is impossible because they are already unioned 
         
         if (rank[p1] > rank[p2]) {
             parent[p2] = p1;
@@ -36,7 +37,7 @@ var findRedundantConnection = function(edges) {
             parent[p1] = p2;
             rank[p2] += rank[p1];
         }
-        
-        return true; //Was able to unify
+        return true;
     }
+    
 };
