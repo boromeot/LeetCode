ac9d600c9d8fba1acb7f197f8a965c7f1f4df955
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-07-07 03:04:15 +0000

    Sync LeetCode submission - Jump Game (javascript)

---
+++ problems/jump_game/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let g = nums.length - 1;
+    
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] + i >= g) {
+            g = i;
+        }
+        if (g === 0) return true;
+    }
+    return false;
+};
