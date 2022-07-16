59eaae56ed3870e99bd5fd5caae6434b34d2cbb7
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-07-16 16:54:45 +0000

    Sync LeetCode submission - House Robber (javascript)

--- problems/house_robber/solution.js
+++ problems/house_robber/solution.js
@@ -3,13 +3,15 @@
  * @return {number}
  */
 var rob = function(nums) {
-    let rob1 = 0,
-        rob2 = 0;
-    
-    for (let i = 0; i < nums.length; i++) {
-        let temp = rob2;
-        rob2 = Math.max(rob1 + nums[i], rob2);
-        rob1 = temp;
+    if (nums.length === 1) {
+        return nums[0];
     }
-    return rob2;
-};
+    
+    let dp = new Array(nums.length)
+    for (let i = 0; i < nums.length; i++) {
+        let n1 = dp[i - 2] || 0,
+            n2 = dp[i - 3] || 0;
+        dp[i] = nums[i] + Math.max(n1, n2);
+    }
+    return Math.max(dp[nums.length - 1], dp[nums.length - 2]);
+}; 
