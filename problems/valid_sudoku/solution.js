d11c6d23ce5568931817402cb5e17a6a3bfc4463
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-09-27 06:05:08 +0000

    Sync LeetCode submission - Valid Sudoku (javascript)

--- problems/valid_sudoku/solution.js
+++ problems/valid_sudoku/solution.js
@@ -3,34 +3,29 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    const ROWS = board.length;
-    const COLS = board[0].length;
+    const ROWS = board.length,
+          COLS = board[0].length;
     
     for (let i = 0; i < ROWS; i++) {
-        const rowSet = new Set();
-        const colSet = new Set();
-        const boxSet = new Set();
+        let rowSet = new Set(),
+            colSet = new Set(),
+            boxSet = new Set();
         
         for (let j = 0; j < COLS; j++) {
-            let r = (Math.floor(i / 3) * 3) + Math.floor(j / 3);
-            let c = 3 * (i % 3) + (j % 3);
+            let x = (j % 3) + (3 * (i % 3)),
+                y = (Math.floor(j / 3)) + (Math.floor(i / 3) * 3);
             
-            if (board[i][j] !== '.') {
-                if (rowSet.has(board[i][j])) return false;
-                rowSet.add(board[i][j]);
+            if (rowSet.has(board[i][j]) ||
+                colSet.has(board[j][i]) ||
+                boxSet.has(board[y][x])) {
+                return false;
             }
-            
-            if (board[j][i] !== '.') {
-                if (colSet.has(board[j][i])) return false;
-                colSet.add(board[j][i]);
-            }
-            
-            if (board[r][c] !== '.') {
-                if (boxSet.has(board[r][c])) return false;
-                boxSet.add(board[r][c]);
-            }
-            
+            if (board[i][j] !== '.') rowSet.add(board[i][j]);
+            if (board[j][i] !== '.') colSet.add(board[j][i]);
+            if (board[y][x] !== '.') boxSet.add(board[y][x]);
         }
     }
-    return true;
+    return true
 };
+
+
