d3f84480509f69485795fd52cbbac52507fd3f3e
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-10-19 11:41:19 -0700

    Time: 63 ms (95.00%), Space: 42.7 MB (47.00%) - LeetHub

--- 339-nested-list-weight-sum/339-nested-list-weight-sum.js
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.js
@@ -41,18 +41,18 @@
  * @return {number}
  */
 var depthSum = function(nestedList) {
-    let sum = 0;
-    dfs(nestedList, 1);
-    return sum;
+    return dfs(nestedList, 1);
     
     function dfs(list, depth) {        
+        let sum = 0;
         for (let n of list) {
             if (n.isInteger()) {
                 sum += n.getInteger() * depth;
             } else {
-                dfs(n.getList(), depth + 1);
+                sum += dfs(n.getList(), depth + 1);
             }
         }
+        return sum;
     }
     
 };
