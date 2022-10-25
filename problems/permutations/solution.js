7959044bb881cf17d1c774e779c7a47c1d3f3f26
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-10-25 22:44:34 +0000

    Sync LeetCode submission - Permutations (javascript)

--- problems/permutations/solution.js
+++ problems/permutations/solution.js
@@ -3,25 +3,19 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    const res = [];
-    dfs([], new Array(nums.length).fill(false));
-    return res;
-    function dfs(perm, used) {
-        if (perm.length === nums.length) {
-            res.push(perm.slice());
-            return;
-        }
+    if (nums.length === 1) return [nums.slice()];
+    let res = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        let n = nums.shift();
+        let perms = permute(nums);
         
-        for (let i = 0; i < nums.length; i++) {
-            if (!used[i]) {
-                perm.push(nums[i]);
-                used[i] = true;
-                
-                dfs(perm, used);
-                
-                perm.pop();
-                used[i] = false;
-            }             // [1,2]
+        for (let perm of perms) {
+            perm.push(n);
         }
+        res.push(...perms);
+        nums.push(n);
+        
     }
+    return res;
 };
