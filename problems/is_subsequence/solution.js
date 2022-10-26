4226094ae41f2313d6a1c2b854764604bf572ad8
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-10-26 18:58:05 +0000

    Sync LeetCode submission - Is Subsequence (javascript)

--- problems/is_subsequence/solution.js
+++ problems/is_subsequence/solution.js
@@ -3,14 +3,18 @@
  * @param {string} t
  * @return {boolean}
  */
-var isSubsequence = function(s, t) {
+var isSubsequence = function(s, t) { // Time O(n), Space O(1)
+    if (s.length === 0) return true;
+    
     let l = 0,
         r = 0;
     
     while (r < t.length) {
-        if (s[l] === t[r]) l++;
-        if (l === s.length) return true;
+        if (s[l] === t[r]) {
+            l++;
+            if (l >= s.length) return true;
+        }
         r++;
     }
-    return l === s.length;
+    return false;
 };
