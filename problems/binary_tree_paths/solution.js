ba5e49a0dd2198d1d52b710a2bb17940b7d5d2f6
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-10-28 20:32:45 +0000

    Sync LeetCode submission - Binary Tree Paths (javascript)

---
+++ problems/binary_tree_paths/solution.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    let res = [];
+    bt(root, '');
+    return res;
+    
+    function bt(root, path) {
+        if (!root) return;
+        if (!root.left && !root.right) {
+            path += root.val;
+            res.push(path);
+            return;
+        }
+        bt(root.left, path + root.val + '->');
+        bt(root.right, path + root.val + '->');
+    }
+};
