2d37fee662b95bcb1f17c5ded541c45443e85796
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-10-31 11:13:36 -0700

    Time: 84 ms (72.76%), Space: 42.6 MB (20.96%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let max = -Infinity;
+    
+    for (let i = 0; i < accounts.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < accounts[i].length; j++) {
+            sum += accounts[i][j];
+        }
+        max = Math.max(max, sum);
+    }
+    return max;
+};
