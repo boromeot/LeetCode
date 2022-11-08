3687f26108a8df18ce1a2a6b21a8c2ba24db7d8b
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-11-07 16:00:13 -0800

    Time: 107 ms (64.74%), Space: 43.8 MB (96.68%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -3,9 +3,12 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    nums.sort((a, b) => a - b);
+    let realSum = nums.length,
+        actSum = 0;
+    
     for (let i = 0; i < nums.length; i++) {
-        if (i !== nums[i]) return i;
+        realSum += i;
+        actSum += nums[i];
     }
-    return nums.length;
+    return realSum - actSum;
 };
