88a8b22734c2a48c1aebda8b256aa05a98b80663
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-11-21 20:03:12 -0800

    Time: 164 ms (31.25%), Space: 52.1 MB (6.87%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let set = new Set();
+    
+    let curr = headA;
+    while (curr) {
+        set.add(curr);
+        curr = curr.next;
+    }
+    
+    curr = headB;
+    while (curr) {
+        if (set.has(curr)) {
+            return curr;
+        }
+        set.add(curr);
+        curr = curr.next;
+    }
+    return null;
+};
