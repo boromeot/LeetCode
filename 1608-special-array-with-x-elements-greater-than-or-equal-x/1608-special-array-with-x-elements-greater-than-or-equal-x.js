df9fde91c35379d7a6a6fdd4abac4b81e1ca7148
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-12-03 12:05:08 -0800

    Time: 106 ms (40.64%), Space: 44.1 MB (24.52%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var specialArray = function(nums) {
+    for (let x = 1; x <= nums.length; x++) {
+        let count = x;
+        for (let n of nums) {
+            if (count < 0) break;
+            if (n >= x) count--;
+        }
+        if (count === 0) return x;
+    }
+    return -1;
+};
