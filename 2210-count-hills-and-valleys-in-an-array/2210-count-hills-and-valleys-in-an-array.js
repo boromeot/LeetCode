7d8bb61922b26081a682a2cba8ec3c994246807b
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2022-12-05 11:26:10 -0800

    Time: 95 ms (58.06%), Space: 41.7 MB (95.70%) - LeetHub

---
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countHillValley = function(nums) {
+    let prev = nums[0];
+    let hills = 0,
+        valleys = 0;
+    
+    for (let i = 1; i < nums.length - 1; i++) {
+        const num = nums[i];
+        if (num === prev) {
+            continue;
+        }
+        
+        if (prev < num && num > nums[i + 1]) {
+            hills++;
+            prev = num;
+        } else if (prev > num && num < nums[i + 1]) {
+            valleys++;
+            prev = num;
+        }
+    }
+    return hills + valleys;
+};
