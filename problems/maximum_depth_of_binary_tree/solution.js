2c76fd490f9726e18dfa86d83ace22cfa4ffc562
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-01-04 19:40:23 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (javascript)

--- problems/maximum_depth_of_binary_tree/solution.js
+++ problems/maximum_depth_of_binary_tree/solution.js
@@ -12,17 +12,15 @@
  */
 var maxDepth = function(root) {
     if (!root) return 0;
-    let maxDepth = 1;
-    dfs(root, maxDepth);
-    return maxDepth;
-    
-    
-    function dfs(node, depth) {
-        if (!node) {
-            return;
-        }
-        maxDepth = Math.max(maxDepth, depth);
-        dfs(node.left, depth + 1);
-        dfs(node.right, depth + 1);
-    }
+
+    let l = maxDepth(root.left),
+        r = maxDepth(root.right);
+
+    if (!root.left && !root.right) return 1;
+
+    if (!root.left) return 1 + r;
+
+    if (!root.right) return 1 + l;
+
+    return Math.max(l, r) + 1;
 };
