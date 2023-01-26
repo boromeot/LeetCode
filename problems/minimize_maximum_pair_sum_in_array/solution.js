94c8d4b2ff85594ce7b01bc8c9c1d9d705225a55
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-01-26 01:40:26 +0000

    Sync LeetCode submission - Minimize Maximum Pair Sum in Array (javascript)

---
+++ problems/minimize_maximum_pair_sum_in_array/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minPairSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    let max = 0;
+    let l = 0, r = nums.length - 1;
+    while (l < r) {
+        max = Math.max(max, nums[l] + nums[r]);
+        l++, r--;
+    }
+    return max;
+};
