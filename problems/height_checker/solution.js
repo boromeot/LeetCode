c3bfe02db8abe78f312dc2a2dd78f5bdb8c8581b
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-01-26 18:59:06 +0000

    Sync LeetCode submission - Height Checker (javascript)

---
+++ problems/height_checker/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    const expected = heights.slice();
+    heights.sort((a, b) => a - b);
+    let res = 0;
+    for (let i = 0; i < heights.length; i++) {
+        if (heights[i] !== expected[i]) res++;
+    }
+    return res;
+};
