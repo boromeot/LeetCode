af3b6612297ba30830acac41b07fd6e357a04737
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-03-12 19:59:12 +0000

    Sync LeetCode submission - To Lower Case (javascript)

---
+++ problems/to_lower_case/solution.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    const lower = new Set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']);
+    return s.toLowerCase();
+};
