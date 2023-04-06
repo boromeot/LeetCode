2d64e394b8eb6d1f0640fcba60ad7ad7567b843e
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-04-06 18:38:10 +0000

    Sync LeetCode submission - Find K Closest Elements (javascript)

---
+++ problems/find_k_closest_elements/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function (arr, k, x) {
+  let leftPointer = 0;
+  let rightPointer = arr.length - 1;
+  while (rightPointer - leftPointer >= k) {
+    // a-x <= b-x (Comparison Law at the Question)
+    if (Math.abs(arr[leftPointer] - x) <= Math.abs(arr[rightPointer] - x)) rightPointer--;
+    else leftPointer++;
+  }
+  return arr.slice(leftPointer, rightPointer + 1);
+};
