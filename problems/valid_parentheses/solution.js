f67543004c417679d8abd0cefe3726e387dd5f70
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-04-10 17:49:24 +0000

    Sync LeetCode submission - Valid Parentheses (javascript)

--- problems/valid_parentheses/solution.js
+++ problems/valid_parentheses/solution.js
@@ -4,22 +4,29 @@
  */
 var isValid = function(s) {
     const stack = [];
+
     const map = {
-        '(' : ')',
-        '[' : ']',
-        '{' : '}'
-    }
-    
-    
+        ')' : '(',
+        ']' : '[',
+        '}' : '{'
+    };
+
     for (let c of s) {
-        if (c === '(' || c === '[' || c === '{') {
-            stack.push(c);
-        } else {
-            if (map[stack.at(-1)] !== c) {
-                return false;            
+        if (map[c]) {
+            if (stack.at(-1) === map[c]) {
+                stack.pop();
+            } else {
+                return false;
             }
-            stack.pop();
-        } 
+        } else {
+            stack.push(c);
+        }
     }
     return stack.length === 0;
 };
+
+// 1. (())()
+
+// 2. )))))
+
+// 
