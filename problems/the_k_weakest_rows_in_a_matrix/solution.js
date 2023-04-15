d1bcf030a1e16863f25ed73f934a903c2f26bc6b
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-04-15 18:45:37 +0000

    Sync LeetCode submission - The K Weakest Rows in a Matrix (javascript)

---
+++ problems/the_k_weakest_rows_in_a_matrix/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+var kWeakestRows = (mat, k) => {
+  const m = mat.length;
+  const n = mat[0].length;
+  const ret = new Uint8Array(k);
+  const visited = new Uint8Array(m);
+  let idx = 0;
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < m; ++j) {
+      if (visited[j] === 0 && mat[j][i] === 0) {
+        ret[idx] = j;
+        visited[j] = 1;
+        if (++idx === k) return ret;
+      }
+    }
+  }
+  for (let i = 0; i < m; ++i) {
+    if (visited[i] === 0) {
+      ret[idx] = i;
+      if (++idx === k) return ret;
+    }
+  }
+};
