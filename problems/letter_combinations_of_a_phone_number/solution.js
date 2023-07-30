544be10ff41e074362ad10cf00a41aabc3e31335
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-07-30 03:44:46 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (javascript)

--- problems/letter_combinations_of_a_phone_number/solution.js
+++ problems/letter_combinations_of_a_phone_number/solution.js
@@ -3,37 +3,36 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
+    if (!digits.length) return [];
+
+    const map = {
+        2: ['a','b','c'],
+        3: ['d','e','f'],
+        4: ['g','h','i'],
+        5: ['j','k','l'],
+        6: ['m','n','o'],
+        7: ['p','q','r','s'],
+        8: ['t','u','v'],
+        9: ['w','x','y','z'],
+    }
+
     const res = [];
-    const map = new Map([
-        ['2', ['a', 'b', 'c']],
-        ['3', ['d', 'e', 'f']],
-        ['4', ['g', 'h', 'i']],
-        ['5', ['j', 'k', 'l']],
-        ['6', ['m', 'n', 'o']],
-        ['7', ['p', 'q', 'r', 's']],
-        ['8', ['t', 'u', 'v']],
-        ['9', ['w', 'x', 'y', 'z']]
-    ]);
-    
-    
-    let combo = '';
-    dfs(0);
+    const combo = [];
+
+    bt(0);
     return res;
-    
-    function dfs(i) {
-        if (i >= digits.length) {
-            if (combo) {
-                res.push(combo);
-            }
+
+    function bt(i) {
+        if (i === digits.length) {
+            res.push(combo.join(''));
             return;
         }
-        for (let j = 0; j < map.get(digits[i]).length; j++) {
-            combo += map.get(digits[i])[j];
-            dfs(i + 1);
-            combo = combo.slice(0, combo.length - 1);
+
+        for (let j = 0; j < map[digits[i]].length; j++) {
+            combo.push(map[digits[i]][j]);
+            bt(i + 1);
+            combo.pop();
         }
-        
+        return;
     }
-    
-    
 };
