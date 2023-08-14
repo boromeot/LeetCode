201d3f0dad5c4add81b15200f70688a7477dffbe
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-08-14 06:43:24 +0000

    Sync LeetCode submission - Target Sum (javascript)

--- problems/target_sum/solution.js
+++ problems/target_sum/solution.js
@@ -5,17 +5,17 @@
  */
 var findTargetSumWays = function(nums, target) {
     let res = 0;
+
     bt(0, 0);
     return res;
-    
+
     function bt(i, sum) {
-        if (i >= nums.length) {
+        if (i === nums.length) {
             if (sum === target) res++;
             return;
         }
-        
+
         bt(i + 1, sum + nums[i]);
         bt(i + 1, sum - nums[i]);
     }
-    
 };
