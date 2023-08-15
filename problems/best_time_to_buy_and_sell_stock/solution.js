a382832c2410e8176be137862e62270acb105338
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-08-15 04:53:28 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (javascript)

--- problems/best_time_to_buy_and_sell_stock/solution.js
+++ problems/best_time_to_buy_and_sell_stock/solution.js
@@ -3,18 +3,11 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let profit = 0;
+    let res = 0;
     let min = prices[0];
-    
-    for (let i = 1; i < prices.length; i++) {
-        profit = Math.max(profit, prices[i] - min);
-        if (prices[i] < min) {
-            min = prices[i];
-        }
+    for (let i = 0; i < prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        res = Math.max(res, prices[i] - min);
     }
-    
-    return profit;
+    return res;
 };
-
-// [7,1,5,3,6,4] output = 5
-//    m   i
