6b16487fad81afd9888fd35e902890f6397136ad
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-08-28 16:09:38 +0000

    Sync LeetCode submission - Valid Perfect Square (javascript)

--- problems/valid_perfect_square/solution.js
+++ problems/valid_perfect_square/solution.js
@@ -3,16 +3,17 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    let l = 1,
+    let l = 0,
         r = num;
     
     while (l <= r) {
-        let m = Math.floor((l + r) / 2);
-        
+        const m = l + Math.floor((r - l) / 2);
         if (m * m === num) return true;
+
         if (m * m < num) {
             l = m + 1;
         }
+
         if (m * m > num) {
             r = m - 1;
         }
