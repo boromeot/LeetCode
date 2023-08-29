95a4253eecb51b99adfd1216d929e5ee4e867116
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-08-29 16:11:43 +0000

    Sync LeetCode submission - Search a 2D Matrix (javascript)

--- problems/search_a_2d_matrix/solution.js
+++ problems/search_a_2d_matrix/solution.js
@@ -2,52 +2,43 @@
  * @param {number[][]} matrix
  * @param {number} target
  * @return {boolean}
- *//* T = 13
-[ 1, 3, 5, 7] 0
-[10,11,16,20] 1 m
-[23,30,34,60] 2
-  0  1  2  3
-*/
+ */
 var searchMatrix = function(matrix, target) {
-    const ROWS = matrix.length,
-          COLS = matrix[0].length;
-    //First find which row its in
-    let rowL = 0,
-        rowR = ROWS - 1;
-    let targetRow;
-    while (rowL <= rowR) {
-        let rowM = Math.floor((rowL + rowR) / 2);
-        
-        let leftNum = matrix[rowM][0];
-        let rightNum = matrix[rowM][COLS - 1];
-        
-        if (leftNum <= target && target <= rightNum) {
-            targetRow = rowM;
-            break;
-        } else if (target < leftNum) {
-            rowR = rowM - 1;
-        } else if (target > rightNum) {
-            rowL = rowM + 1;
-        }
-    }
+    const rows = matrix.length;
+    const cols = matrix[0].length;
     
+    let l = 0, r = rows -1;
+
+    let targetRow;
+
+    while (l <= r) {
+        const m = l + Math.floor((r - l) / 2);
+        const leftNum = matrix[m][0],
+              rightNum = matrix[m][cols - 1];
+        if (leftNum <= target && target <= rightNum) {
+            targetRow = m;
+            break;
+        }
+
+        if (leftNum > target) r = m - 1;
+        if (rightNum < target) l = m + 1;
+    }
+
     if (targetRow === undefined) return false;
     
-    let l = 0,
-        r = COLS - 1;
-    
+    l = 0, r = cols - 1;
     while (l <= r) {
-        let m = Math.floor((r + l) / 2);
-        let num = matrix[targetRow][m];
-        if (num < target) {
-            l = m + 1;
-        } else if (num > target) {
-            r = m - 1;
-        } else {
-            return true;
-        }
+        const m = l + Math.floor((r - l) / 2);
+        const num = matrix[targetRow][m];
+        if (num === target) return true;
+
+        if (num < target) l = m + 1;
+        if (num > target) r = m - 1;
     }
     return false;
-};
+}
 
-
+//    0, 1, 2, 3
+// 0  0, 1, 2, 3
+// 1  4, 5, 6, 7
+// 2  8, 9, 10, 11
