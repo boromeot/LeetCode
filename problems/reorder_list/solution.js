b2158e060c1d9a737482f6192964d3d2085b7ac2
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-09-22 18:01:26 +0000

    Sync LeetCode submission - Reorder List (javascript)

--- problems/reorder_list/solution.js
+++ problems/reorder_list/solution.js
@@ -10,61 +10,23 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function(head) {
-    let slow = head,
-        fast = head.next;
-    //Find the mid point of the list
-    while (fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;
-    }
-    let temp = slow.next;
-    slow.next = null;
-    let mid = temp;
-    
-    // Reverse the second half of the list
-    let prev = null;
-    while (mid) {
-        const newHead = mid.next;
-        mid.next = prev;
-        prev = mid;
-        mid = newHead;
-    }
-
-    let head2 = prev;
+    let arr = [];
     let curr = head;
-    
-    // Merge the two list
-    while (head2) {
-        let t = curr.next,
-            t2 = head2.next;
-        
-        curr.next = head2;
-        head2.next = t;
-        curr = t;
-        head2 = t2;
+    while (curr) {
+        arr.push(curr);
+        curr = curr.next;
     }
-    
-    return head;
-}
-/*
 
-1 -> 2 -> 3 -> 4 -> 5 -> 6
-s
-     f
-     s         f
-          s              f 
-1 -> 6 -> 2 -> 5 -> 3 -> 4
-1 -> 2 -> 3
-6 -> 5 -> 4
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].next = null;
+    }
 
-1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
-s
-     f
-     s         f
-          s              f
-               s                  f
-1 -> 7 -> 2 -> 6 -> 3 -> 5 -> 4
-1 -> 2 -> 3 -> 4
-7 -> 6 -> 5
-
-*/
+    let l = 0, r = arr.length - 1;
+    while (l < r) {
+        arr[l].next = arr[r];
+        l++;
+        if (l >= r) break;
+        arr[r].next = arr[l];
+        r--;
+    }
+};
