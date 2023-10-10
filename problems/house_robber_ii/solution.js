4ca2cf96d2d2b2ad978960f995c58314094df4ad
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-10-10 16:39:48 +0000

    Sync LeetCode submission - House Robber II (javascript)

--- problems/house_robber_ii/solution.js
+++ problems/house_robber_ii/solution.js
@@ -3,28 +3,20 @@
  * @return {number}
  */
 var rob = function(nums) {
-    if (nums.length === 1 || nums.length === 2) {
-        let n1 = nums[0] || 0;
-        let n2 = nums[1] || 0;
-        return Math.max(n1, n2);
-    }
-    
-    const len = nums.length;
-    
-    let dp = new Array(nums.length);
-    for (let i = 0; i < nums.length - 1; i++) {
-        let n1 = dp[i - 2] || 0;
-        let n2 = dp[i - 3] || 0;
-        dp[i] = nums[i] + Math.max(n1, n2);
-    }
-    let max1 = Math.max(dp[len - 3], dp[len - 2]);
-    
-    let dp2 = new Array(nums.length);
-    for (let i = 1; i < nums.length; i++) {
-        let n1 = dp2[i - 2] || 0;
-        let n2 = dp2[i - 3] || 0;
-        dp2[i] = nums[i] + Math.max(n1, n2);
-    }
-    let max2 = Math.max(dp2[len - 1], dp2[len - 2]);
-    return Math.max(max1, max2);
+    if (nums.length === 1) return nums[0];
+    let arr1 = nums.slice(0, nums.length - 1);
+    let arr2 = nums.slice(1);
+    return Math.max(HR(arr1), HR(arr2));
 };
+
+function HR(nums) {
+    let rob1 = 0,
+        rob2 = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        let temp = rob2;
+        rob2 = Math.max(rob1 + nums[i], rob2);
+        rob1 = temp;
+    }
+    return rob2;
+}
