017238fce6620b60a6218ffbb80388ee5bf8da33
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2023-11-18 02:40:06 +0000

    Sync LeetCode submission - Recyclable and Low Fat Products (mssql)

--- problems/recyclable_and_low_fat_products/solution.sql
+++ problems/recyclable_and_low_fat_products/solution.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 SELECT product_id
 FROM Products
-WHERE Products.low_fats = 'Y' AND Products.recyclable = 'Y';
+WHERE low_fats = 'Y' AND recyclable = 'Y';
