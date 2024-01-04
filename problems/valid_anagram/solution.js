36ce94eaf3e2b5c2413b8f4642938a33f929bc9f
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2024-01-04 23:27:44 +0000

    Sync LeetCode submission - Valid Anagram (javascript)

--- problems/valid_anagram/solution.js
+++ problems/valid_anagram/solution.js
@@ -3,27 +3,17 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {// time O(n), space O(n)
-    let count1 = {};
-    let count2 = {};
-    
-    
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    let count = {};
     for (let c of s) {
-        if (count1[c]) count1[c]++;
-        else count1[c] = 1;
+        count[c] ? count[c]++ : count[c] = 1;
     }
-    
+
     for (let c of t) {
-        if (count2[c]) count2[c]++;
-        else count2[c] = 1;
-    }
-    
-    for (let k of Object.keys(count1)) {
-        if (count1[k] !== count2[k]) return false;
-    }
-    
-    for (let k of Object.keys(count2)) {
-        if (count2[k] !== count1[k]) return false;
+        if (count[c] === undefined) return false;
+        if (count[c] <= 0) return false;
+        count[c]--;
     }
     return true;
 };
