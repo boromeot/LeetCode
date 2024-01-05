fda306476d75f4f0f31c357e92fb70025c966c23
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2024-01-05 20:28:53 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (javascript)

--- problems/longest_consecutive_sequence/solution.js
+++ problems/longest_consecutive_sequence/solution.js
@@ -4,16 +4,14 @@
  */
 var longestConsecutive = function(nums) {
     const set = new Set(nums);
-    
+
     let res = 0;
-    
-    for (let n of set.values()) {
-        if (set.has(n - 1)) continue; // This means n is not the beggining of a sequence
+    for (const n of set) {
+        if (set.has(n - 1)) continue;
         let count = 1;
-        let cur = n;
-        
-        while (set.has(cur + 1)) {
-            cur++;
+        let num = n;
+        while (set.has(num + 1)) {
+            num++;
             count++;
         }
         res = Math.max(res, count);
