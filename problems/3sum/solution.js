5e02a2c2dc068a857e86004c9cd79a7eaec6d009
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2024-01-06 00:53:18 +0000

    Sync LeetCode submission - 3Sum (javascript)

--- problems/3sum/solution.js
+++ problems/3sum/solution.js
@@ -3,48 +3,27 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    const target = 0;
     const res = [];
     nums.sort((a, b) => a - b);
-    
-    // [-4,-1,-1,-1,0,1,2]
-    //      i  l        r
+
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] > 0) break;
-        
-        let l = i + 1,
-            r = nums.length - 1;
-        
+        let l = i + 1, r = nums.length - 1;
         while (l < r) {
-            let sum = nums[i] + nums[l] + nums[r];
-            if (sum < target) {
-                l++;
-            } else if (sum > target) {
-                r--;
-            } else {
+            const sum = nums[i] + nums[l] + nums[r];
+            if (sum > 0) r--;
+            else if (sum < 0) l++;
+            else {
                 res.push([nums[i], nums[l], nums[r]]);
-                while (nums[l] === nums[l + 1] && l < r) {
-                    l++;
-                }
-                while (nums[r] === nums[r - 1] && r > l) {
-                    r--;
-                }
+                while (nums[l] === nums[l + 1] && l < r) l++;
+                while (nums[r] === nums[r - 1] && l < r) r--;
                 l++;
                 r--;
             }
         }
-        while (nums[i] === nums[i + 1] && i < nums.length) {
+            while (nums[i] === nums[i + 1] && i < nums.length) {
             i++;
         }
-        
     }
     return res;
-    // [0]
-    //  i  l  r
-    
-
-    
-    // a + b + c = 0
-    // b + c = -a
-    
 };
