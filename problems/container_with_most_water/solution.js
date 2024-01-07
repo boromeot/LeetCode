d297fc3d7ed2a84d8566edd78142ab3949c25a6c
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2024-01-07 20:15:35 +0000

    Sync LeetCode submission - Container With Most Water (javascript)

--- problems/container_with_most_water/solution.js
+++ problems/container_with_most_water/solution.js
@@ -2,32 +2,13 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(heights) {
-    let max = 0;
-    
-    let l = 0,
-        r = heights.length - 1;
-    
+var maxArea = function(height) {
+    let l = 0, r = height.length - 1;
+    let res = 0;
     while (l < r) {
-        const height = Math.min(heights[l], heights[r]);
-        const width = r - l;
-        max = Math.max(max, height * width);
-        
-        if (heights[l] < heights[r]) {
-            l++;
-        } else {
-            r--;
-        }
+        res = Math.max(res, Math.min(height[l], height[r]) * (r - l));
+        if (height[l] < height[r]) l++;
+        else r--;
     }
-    
-    return max;
+    return res;
 };
-
-// [1,8,6,2,5,4,8,3,7]
-//    l           r
-
-// []
-
-// area = height * width
-// height is Math.min(leftBar, rightBar)
-// width is distance between them => r - l
