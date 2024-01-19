157363b491104d527c0d95e50e7d00f8755af3ea
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2024-01-19 00:45:23 +0000

    Sync LeetCode submission - Car Fleet (javascript)

--- problems/car_fleet/solution.js
+++ problems/car_fleet/solution.js
@@ -5,25 +5,22 @@
  * @return {number}
  */
 var carFleet = function(target, position, speed) {
-    const stack = [];
-    let max = -Infinity;
-    
-    let cars = []; // [position, velocity]
+    const cars = new Array(position.length); // [position, speed];
+
     for (let i = 0; i < position.length; i++) {
-        cars[i] = [position[i], speed[i]];
-    }
-    
-    cars.sort((a, b) => a[0] - b[0]); // Sort based on position
-    
+        cars[i] = [position[i], speed[i]]; 
+    }    
+    cars.sort((a, b) => a[0] - b[0]);
+
+    let max = -Infinity;
+    const stack = [];
     for (let i = cars.length - 1; i >= 0; i--) {
-        const [pos, vel] = cars[i];
-        let t = (target - pos) / vel;
-        if (t > max) {
-            max = t;
-            stack.push(t);
+        const [pos, speed] = cars[i];
+        const time = (target - pos) / speed;
+        if (time > max) {
+            stack.push(time);
+            max = time;
         }
     }
-    
-    
     return stack.length;
 };
