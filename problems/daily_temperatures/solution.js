e4f93f915dc3fa14c8e7492624ed4829f4750f41
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2024-01-19 00:10:17 +0000

    Sync LeetCode submission - Daily Temperatures (javascript)

--- problems/daily_temperatures/solution.js
+++ problems/daily_temperatures/solution.js
@@ -3,15 +3,14 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temps) {
-    const stack = [];
     const res = new Array(temps.length).fill(0);
+    const stack = [];
 
     for (let i = 0; i < temps.length; i++) {
         const temp = temps[i];
 
         while (temp > temps[stack.at(-1)]) {
             const oldDay = stack.pop();
-
             res[oldDay] = i - oldDay;
         }
         stack.push(i);
