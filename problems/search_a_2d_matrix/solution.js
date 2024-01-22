df8e90ff704983a9e945cbf9d4390bfd60a24cf8
Tyler Boromeo <58971053+boromeot@users.noreply.github.com>
2024-01-22 20:36:49 +0000

    Sync LeetCode submission - Search a 2D Matrix (javascript)

--- problems/search_a_2d_matrix/solution.js
+++ problems/search_a_2d_matrix/solution.js
@@ -4,41 +4,30 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    const rows = matrix.length;
-    const cols = matrix[0].length;
+    const rowIndex = findRow(matrix, target);
+    const row = matrix[rowIndex];
     
-    let l = 0, r = rows -1;
-
-    let targetRow;
+    let l = 0; r = row.length - 1;
 
     while (l <= r) {
         const m = l + Math.floor((r - l) / 2);
-        const leftNum = matrix[m][0],
-              rightNum = matrix[m][cols - 1];
-        if (leftNum <= target && target <= rightNum) {
-            targetRow = m;
-            break;
-        }
-
-        if (leftNum > target) r = m - 1;
-        if (rightNum < target) l = m + 1;
-    }
-
-    if (targetRow === undefined) return false;
-    
-    l = 0, r = cols - 1;
-    while (l <= r) {
-        const m = l + Math.floor((r - l) / 2);
-        const num = matrix[targetRow][m];
-        if (num === target) return true;
-
-        if (num < target) l = m + 1;
-        if (num > target) r = m - 1;
+        const n = row[m];
+        if (n === target) return true;
+        else if (n < target) l = m + 1;
+        else if (n > target) r = m - 1;
     }
     return false;
-}
+};
 
-//    0, 1, 2, 3
-// 0  0, 1, 2, 3
-// 1  4, 5, 6, 7
-// 2  8, 9, 10, 11
+function findRow(matrix, target) {
+    let l = 0, r = matrix.length - 1;
+    let m;
+    while (l <= r) {
+        m = l + Math.floor((r - l) / 2);
+        const [start, end] = [matrix[m].at(0), matrix[m].at(-1)];
+        if (start <= target && target <= end) break;
+        else if (end < target) l = m + 1;
+        else if (target < start) r = m - 1;
+    }
+    return m;
+}
